22e3255bcc0176a4a0aa5388a8e686159c6ab888
devtiagom <tiago.o.machado@gmail.com>
2021-04-03 22:47:05 -0300

    Teste E2e: Tentar excluir uma reserva que não existe

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/DeleteBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/DeleteBookingTest.java
@@ -23,6 +23,12 @@ public class DeleteBookingTest extends BaseTest {
     GetBookingRequest getBookingRequest = new GetBookingRequest();
     DeleteBookingRequest deleteBookingRequest = new DeleteBookingRequest();
 
+    int primeiroId = getBookingRequest.allBookings()
+            .then()
+            .statusCode(200)
+            .extract()
+            .path("[0].bookingid");
+
     // Este teste falha porque a API está retornando status http 201 (Created),
     // e me pareceu mais coerente esperar um status 200 (Ok). Neste caso reportaria um bug.
     @Test
@@ -30,12 +36,6 @@ public class DeleteBookingTest extends BaseTest {
     @Category(Acceptance.class)
     @DisplayName("Deletar uma reserva utilizando token")
     public void validarDeletarUmaReservaUtilizandoToken() {
-        int primeiroId = getBookingRequest.allBookings()
-                .then()
-                .statusCode(200)
-                .extract()
-                .path("[0].bookingid");
-
         deleteBookingRequest.deletarUmaReservaComToken(primeiroId).then()
                 .statusCode(200)
                 .time(lessThan(2L), TimeUnit.SECONDS)
@@ -47,15 +47,22 @@ public class DeleteBookingTest extends BaseTest {
     @Category(E2e.class)
     @DisplayName("Tentar deletar uma reserva sem autorização")
     public void validarDeletarUmaReservaSemToken() {
-        int primeiroId = getBookingRequest.allBookings()
-                .then()
-                .statusCode(200)
-                .extract()
-                .path("[0].bookingid");
-
         deleteBookingRequest.deletarUmaReservaSemToken(primeiroId).then()
                 .statusCode(403)
                 .time(lessThan(2L), TimeUnit.SECONDS)
                 .body(containsString("Forbidden"));
+    }
+
+    // Este teste falha porque a API está retornando status http 405 (Method Not Allowed),
+    // e me pareceu mais coerente esperar um status 401 (Not Found). Neste caso reportaria um bug.
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Category(E2e.class)
+    @DisplayName("Tentar deletar uma reserva que não existe")
+    public void validarDeletarUmaReservaInvalidaUtilizandoToken() {
+        deleteBookingRequest.deletarUmaReservaComToken(1234).then()
+                .statusCode(401)
+                .time(lessThan(2L), TimeUnit.SECONDS)
+                .body(containsString("Not Found"));
     }
 }
